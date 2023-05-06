0829a7e73e67e18c3b3773277d0b6f7dde0e5be8
azertgyv <atagiyev077@gmail.com>
2023-05-06 21:33:17 +0400

    laptop

---
+++ app.js
@@ -0,0 +1,73 @@
+// const laptop = {
+//     name: 'Asus',
+//     price: 1000,
+// }
+// laptop.price -= 200
+// console.log(laptop)
+
+
+// const product = {
+//     name: 'Honor',
+//     price: 1000,
+//     owner: 'Ali',
+//     ownerCountry: {
+//         name: 'Azerbaijan',
+//         capital: 'Baku'
+//     },
+//     key: 'Azer'
+// }
+
+// // console.log(product.price)
+// // let key =prompt('ureyin hansi keyi isteyir? (name,price,owner')
+// // console.log(product[key])
+// // console.log(product.key)
+
+// for (let key in product) {
+//     console.log(product[key])
+// }
+
+
+// let salaries = {
+//     john: 100,
+//     ann: 160,
+//     peter: 130,
+//     azer: 300
+// }
+// let sum = 0;
+
+// for (let key in salaries) {
+//     sum += salaries[key]
+// }
+// console.log(sum)
+
+// let n1 = 'ali'
+// let n2 = n1
+// n1 = 'ahmad'
+
+// console.log(n1)
+// console.log(n2)
+
+// // value[primitive] -> String, Number, boolean, undefined
+// // referance(non - primitive) -> Object, array, function
+
+// let d1 = { ad: 'ali' }
+// let d2 = d1
+// d1.ad = 'ahmad'
+// console.log(d1.ad)
+// console.log(d2.ad)
+
+
+// const sezar = {
+//     name: 'sezar',
+//     age: 2005
+// }
+
+// // spread
+// // const sezar2 = Object.assign({}, sezar)
+// const sezar2 = { ...sezar }
+// sezar.name = 'yuli'
+// console.log(sezar2)
+
+
+
+
